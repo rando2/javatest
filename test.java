f9109e6dad87b968c7f1a98aff5757ed99fd9a6f
HM Rando <hrando@smith.edu>
2023-09-05 10:35:52 -0400

    add test

---
+++ test.java
@@ -0,0 +1,12 @@
+/** A class containing a variety of methods on arrays */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public static float wholeSum(float[] arr){
+        return 4f;
+
+    }
+}
+
